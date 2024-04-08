3f69b63e40ce9022ec7aae5c4406093cf8282800
zhoushenghui <1727711853@qq.com>
2024-04-08 13:48:06 +0800

    添加自定义图标

--- src/Applications/__/main.cpp
+++ src/Applications/__/main.cpp
@@ -2,25 +2,42 @@
 #include <vtkAutoInit.h>
 #include "gui.h"
 #include "guiframe.h"
-
-
+#include <QFileDialog>
+#include <qtemporarydir.h>
 VTK_MODULE_INIT(vtkRenderingOpenGL2);
 VTK_MODULE_INIT(vtkInteractionStyle);
 VTK_MODULE_INIT(vtkRenderingFreeType);
 VTK_MODULE_INIT(vtkRenderingVolumeOpenGL2);
-
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkNew.h>
+#include <vtkSmartPointer.h>
 int main(int argc, char* argv[])
 {
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 	QApplication application(argc, argv);
 	//vtkObject::GlobalWarningDisplayOff();
 	application.setWindowIcon(QIcon(iconapp));
-        //MainWindow w;
-        //w.show();
-	asclepios::gui::GUIFrame guiFrame;
+	/*asclepios::gui::GUIFrame guiFrame;
 	asclepios::gui::GUI gui;
 	guiFrame.setContent(&gui);
-        guiFrame.show();
-	//guiFrame.showMaximized();
+        guiFrame.show();*/
+        vtkNew<vtkRenderWindow> rw;
+        vtkNew<vtkRenderer> rend;
+        rw->AddRenderer(rend);
+        vtkNew<vtkRenderWindowInteractor> inter;
+        rw->SetInteractor(inter);
+        rw->Render();
+        rw->SetCurrentCursor(VTK_CURSOR_CUSTOM);
+        QTemporaryDir tempDir;
+        if (tempDir.isValid()) {
+            const QString tempFile = tempDir.path() + "/rotate1.cur";
+            if (QFile::copy(":/res/rrotate.cur", tempFile)) {
+                rw->SetCursorFileName(tempFile.toStdString().c_str());
+            }
+        }
+        inter->Initialize();
+        inter->Start();
 	return application.exec();
 }
