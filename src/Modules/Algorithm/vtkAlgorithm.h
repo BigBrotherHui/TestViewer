03f845d0687101417aa5b04954631c1c298a1c34
zhoushenghui <1727711853@qq.com>
2024-04-19 11:13:35 +0800

    添加注释

--- src/Modules/Algorithm/vtkAlgorithm.h
+++ src/Modules/Algorithm/vtkAlgorithm.h
@@ -4,11 +4,13 @@
 #include <Eigen/Eigen>
 #include <vtkPoints.h>
 #include <vtkPolyData.h>
+//use for mpr
 Algorithm_EXPORT void calculateTranslateX(double &x, double &y, double angle);
 Algorithm_EXPORT void calculateTranslateY(double &x, double &y, double angle);
 Algorithm_EXPORT int calculateCursorState(double angle);
+//
 Algorithm_EXPORT void sortVtkPoints(vtkPoints *vtkpoints);
-Algorithm_EXPORT Eigen::MatrixX3d projectPoints(const Eigen::Matrix3Xd &points, const Eigen::Vector4d &parameters);
+Algorithm_EXPORT Eigen::MatrixX3d projectPoints(const Eigen::Matrix3Xd &points, const Eigen::Vector4d &plane);
 Algorithm_EXPORT void ProjectToPlane(const double x[3], const double origin[3], const double normal[3],
                                      double xproj[3]);
 Algorithm_EXPORT Eigen::Vector4d fitPlane(const Eigen::MatrixX3d &points);
